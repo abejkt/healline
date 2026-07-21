63b56a6d47a29c3d2e4dd638083531ebf6dfdec6
abejkt <abe.jkt@gmail.com>
2026-07-21 22:58:23 +0700

    comment const di mock

--- lib/models/mock_database.dart
+++ lib/models/mock_database.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'poli.dart';
 
 class MockDatabase {
   MockDatabase._();
 
-  static const List<String> availableYearFilters = ['2026', '2025'];
-  static const int totalVisitCount = 12;
+//  static const List<String> availableYearFilters = ['2026', '2025'];
+//  static const int totalVisitCount = 12;
 
-  static final DateTime nextVisitDate = DateTime(2026, 5, 15);
+//  static final DateTime nextVisitDate = DateTime(2026, 5, 15);
 }
