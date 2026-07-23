b4b9979309f41a9431d03bd83bffd953fb45f884
abejkt <abe.jkt@gmail.com>
2026-07-23 20:51:03 +0700

    add ;

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -3,7 +3,7 @@ class ApiConfig {
 
   static const String baseUrl = 'https://34.50.106.220:8443/rest/v1';
 //  static const String apiKey = 'sb_publishable_0usGoAC2P6E6nhBGvqM3Xj_KXPDZaod';
-  static const String apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzc5Mjg5MTExLCJleHAiOjE5MzY5NjkxMTF9.AGU9FqRFP-uPjPqvMSeHTmD22GWLecz_qAa5B6fL1Hg'
+  static const String apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzc5Mjg5MTExLCJleHAiOjE5MzY5NjkxMTF9.AGU9FqRFP-uPjPqvMSeHTmD22GWLecz_qAa5B6fL1Hg';
   static const String jwtToken = 'AGNDGFwD3cdj49Je3FHJIV8YQN7R1Cwmgc+GNa6Q';
 
   static Map<String, String> get headers => {
